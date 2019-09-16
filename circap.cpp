f15bcd968afc47f879d670b0a56952a421bd0b0d
Ubuntu <ubuntu@ip-172-31-18-91.ec2.internal>
2019-09-16 21:46:22 +0000

    only code honestl

---
+++ circap.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 myself All rights reserved.
+//
+// Created by: Matsuru Hoshi
+// Created on: Sept 2019
+// This is area periemter circle
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "The radius of circle is: 15mm \n" << std::endl;
+    std::cout << "The area is: " << (M_PI * (pow(15, 2))) << std::endl;
+    std::cout << "The perimeter is: " << (2 * M_PI * 15) << std::endl;
+}
